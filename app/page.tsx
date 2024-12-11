023f6dbe9a03f8062406dbb7940b7c425f0c234a
MatheusKristman <kristman058@gmail.com>
2024-12-11 20:47:21 -0300

    added visuals and animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,137 @@
+"use client";
+
+import { Play } from "lucide-react";
 import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
+
+const container = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const title = {
+  initial: {
+    x: -50,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "circInOut",
+    },
+  },
+};
+
+const label = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "circInOut",
+    },
+  },
+};
+
+const image = {
+  initial: {
+    x: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "circInOut",
+    },
+  },
+};
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="w-full flex flex-col relative min-h-screen">
+      <div className="w-full pt-14 pb-9 flex flex-col gap-9 lg:container lg:flex-grow lg:mx-auto lg:flex-row lg:pb-0 lg:pt-10">
+        <AnimatePresence>
+          <motion.div
+            key="image"
+            variants={container}
+            initial="initial"
+            animate="animate"
+            className="w-full lg:flex lg:flex-col lg:justify-end"
+          >
+            <motion.h1
+              variants={title}
+              className="text-5xl px-6 font-bold !leading-[100%] relative z-10 sm:px-16 sm:text-7xl"
+            >
+              Norem ipsum dolor sit amet
+            </motion.h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <div className="w-full pl-6 grid grid-cols-[20px_1fr] items-end gap-2 -mt-6 sm:pl-16 sm:grid-cols-[30px_1fr]">
+              <motion.div variants={label} className="!-rotate-90 relative h-[20px] sm:h-[30px]">
+                <span className="w-[155px] block uppercase text-xl font-bold !leading-[100%] absolute top-0 left-0 sm:text-3xl sm:w-[233px]">
+                  NOREM IPSUM
+                </span>
+              </motion.div>
+
+              <motion.div variants={image} className="w-full aspect-square relative">
+                <Image src="/gp-mooca.png" alt="GP Mooca" fill className="object-cover object-center" />
+              </motion.div>
+            </div>
+          </motion.div>
+
+          <motion.p
+            key="text"
+            initial={{ y: 100, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, ease: "circInOut" }}
+            className="w-full px-6 text-xl font-semibold !leading-[150%] sm:px-16 lg:mt-[115px]"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Korem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
+            tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit
+            sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac
+            scelerisque ante pulvinar.{" "}
+          </motion.p>
+        </AnimatePresence>
+      </div>
+
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.9, duration: 1, ease: "circInOut" }}
+        className="w-full bg-[#FFDF5C] flex flex-col items-center gap-12 sticky bottom-0"
+      >
+        <div className="w-full pt-12 px-6 flex flex-col items-center gap-8 sm:px-16 sm:flex-row-reverse sm:justify-between">
+          <span className="text-2xl font-bold text-black !leading-[150%] text-center sm:text-right">
+            Norem ipsum dolor sit amet, consectetur adipiscing elit.
+          </span>
+
+          <div className="w-full flex items-center justify-between sm:justify-start sm:gap-9">
+            <button className="border-[3px] border-black rounded-full size-20 flex items-center justify-center">
+              <Play color="#000" size={50} strokeWidth={1.5} />
+            </button>
+
+            <span className="text-4xl font-medium text-black !leading-[150%]">00:00</span>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="w-full">
+          <div className="w-1/2 h-4 bg-black" />
+        </div>
+      </motion.div>
+    </main>
   );
 }
